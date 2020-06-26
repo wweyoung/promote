4536caf9fcdf6d64bebc16cae563c70035adab64
KX <kxiang163@163.com>
2020-06-26 17:17:50 +0800

    Initial commit

---
+++ app/src/main/java/com/kx/promote/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kx.promote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hello promote");
+    }
+}
