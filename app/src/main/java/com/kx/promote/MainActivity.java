04bb451617fb6f9f737814c3be8798e55373ba33
KX <kxiang163@163.com>
2020-06-26 22:40:10 +0800

    更新导航样式，设计界面参考样式来做

--- app/src/main/java/com/kx/promote/MainActivity.java
+++ app/src/main/java/com/kx/promote/MainActivity.java
@@ -2,7 +2,13 @@ package com.kx.promote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kx.promote.ui.CommonTabActivity;
+import com.kx.promote.ui.SlidingTabActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello promote");
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CommonTabActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SlidingTabActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
