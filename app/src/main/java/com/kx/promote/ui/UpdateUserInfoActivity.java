dbe636f16886c2814dda21f16013d1e1886e9cb6
Hyynice <2621032682@qq.com>
2020-06-30 09:07:38 +0800

    修改用户信息界面

---
+++ app/src/main/java/com/kx/promote/ui/UpdateUserInfoActivity.java
@@ -0,0 +1,16 @@
+package com.kx.promote.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kx.promote.R;
+
+public class UpdateUserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user_info);
+    }
+}
